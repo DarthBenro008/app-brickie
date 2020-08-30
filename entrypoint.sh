d461538a22d9d1e1303e887412cf197dd3bb89db
Hemanth Krishna <31009634+DarthBenro008@users.noreply.github.com>
2020-08-30 21:55:16 +0530

    PackageName Addition (#2)
    * feat: renaming scripts
    
    * add: input param in action.yml
    
    * fix: default app name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,33 @@
-#!/bin/sh -l
+#!/bin/bash
 bash ./gradlew test --stacktrace
 bash ./gradlew assembleDebug --stacktrace
-echo $1
-output=$(curl --location --request POST 'http://appbrickie.herokuapp.com/api/sendPackage' \
-  --form 'file=@app/build/outputs/apk/debug/app-debug.apk' \
-  --form 'id='$1'' \
-  --form 'msg=Automated File!')
-echo "::set-output name=result::$output"
+
+packageName="app-debug.apk"
+
+sendpackage(){
+  echo "Package Name to $packageName"
+  output=$(curl --location --request POST 'http://appbrickie.herokuapp.com/api/sendPackage' \
+    --form 'file=@app/build/outputs/apk/debug/'$packageName'' \
+    --form 'id='$INPUT_CHATID'' \
+    --form 'msg=Build Successful https://www.github.com/'$GITHUB_REPOSITORY'/commit/'$GITHUB_SHA'')
+  echo "::set-output name=result::$output"
+}
+
+renamePackage(){
+  {
+    mv app/build/outputs/apk/debug/app-debug.apk app/build/outputs/apk/debug/"$packageName"
+  }||{
+    echo "File Renaming Error Trying , reverting to normal name"
+    packageName="app.apk"
+  }
+
+}
+
+if [ -z "$INPUT_PACKAGENAME" ]
+then
+  sendpackage 
+else
+  packageName="$INPUT_PACKAGENAME.apk"
+  renamePackage
+  sendpackage
+fi 
